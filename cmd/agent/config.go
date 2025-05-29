47328cf6139143a6e74c6d7f1856cd81d7cec283
crazylazyowl <58094513+crazylazyowl@users.noreply.github.com>
2025-05-29 18:59:45 +0200

    fix config intervals

--- cmd/agent/config.go
+++ cmd/agent/config.go
@@ -3,8 +3,14 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
+)
+
+const (
+	minInterval = 1
+	maxInterval = 100
 )
 
 type config struct {
@@ -44,10 +50,10 @@ func loadConfig() (conf *config, err error) {
 	switch {
 	case conf.address == "":
 		err = errors.New("the address is not specified")
-	case conf.reportInterval < 1 || conf.reportInterval > 100:
-		err = errors.New("the report interval value must be between 1 and 99")
-	case conf.pollInterval < 1 || conf.pollInterval > 100:
-		err = errors.New("the poll internval value must be between 1 and 99")
+	case conf.reportInterval < minInterval || conf.reportInterval > maxInterval:
+		err = fmt.Errorf("the report interval must be between %d and %d", minInterval, maxInterval)
+	case conf.pollInterval < minInterval || conf.pollInterval > maxInterval:
+		err = fmt.Errorf("the poll internval must be between %d and %d", minInterval, maxInterval)
 	}
 
 	return
