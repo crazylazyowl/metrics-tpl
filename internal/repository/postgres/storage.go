9571ee811d82d03d91367c52f5d116385ec3b267
crazylazyowl <58094513+crazylazyowl@users.noreply.github.com>
2025-07-07 17:06:47 +0200

    imp fetch many for postgres

--- internal/repository/postgres/storage.go
+++ internal/repository/postgres/storage.go
@@ -61,7 +61,25 @@ func (s *PostgresStorage) Ping(ctx context.Context) error {
 }
 
 func (s *PostgresStorage) Fetch(ctx context.Context) ([]metrics.Metric, error) {
-	return nil, nil
+	stmt, err := s.db.PrepareContext(ctx, `SELECT name, type, counter, gauge FROM metrics;`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.QueryContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	mm := make([]metrics.Metric, 0) // TODO: it's not good
+	for rows.Next() {
+		var m metrics.Metric
+		if err := rows.Scan(&m.ID, &m.Type, &m.Counter, &m.Gauge); err != nil {
+			return nil, err
+		}
+		mm = append(mm, m)
+	}
+	return mm, nil
 }
 
 func (s *PostgresStorage) FetchOne(ctx context.Context, m metrics.Metric) (metrics.Metric, error) {
@@ -81,8 +99,8 @@ func (s *PostgresStorage) FetchOne(ctx context.Context, m metrics.Metric) (metri
 	return m, nil
 }
 
-func (s *PostgresStorage) UpdateOne(ctx context.Context, one metrics.Metric) error {
-	query, err := selectUpsertQuery(one)
+func (s *PostgresStorage) UpdateOne(ctx context.Context, m metrics.Metric) error {
+	query, err := selectUpsertQuery(m)
 	if err != nil {
 		return err
 	}
@@ -91,30 +109,30 @@ func (s *PostgresStorage) UpdateOne(ctx context.Context, one metrics.Metric) err
 		return err
 	}
 	defer stmt.Close()
-	_, err = stmt.ExecContext(ctx, one.ID, one.Type, one.Counter, one.Gauge)
+	_, err = stmt.ExecContext(ctx, m.ID, m.Type, m.Counter, m.Gauge)
 	return err
 }
 
-func (s *PostgresStorage) Update(ctx context.Context, many []metrics.Metric) error {
+func (s *PostgresStorage) Update(ctx context.Context, mm []metrics.Metric) error {
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
-	for _, one := range many {
-		query, err := selectUpsertQuery(one)
+	for _, m := range mm {
+		query, err := selectUpsertQuery(m)
 		if err != nil {
 			return err
 		}
-		if _, err := tx.ExecContext(ctx, query, one.ID, one.Type, one.Counter, one.Gauge); err != nil {
+		if _, err := tx.ExecContext(ctx, query, m.ID, m.Type, m.Counter, m.Gauge); err != nil {
 			return err
 		}
 	}
 	return tx.Commit()
 }
 
-func selectUpsertQuery(metric metrics.Metric) (query string, err error) {
-	switch metric.Type {
+func selectUpsertQuery(m metrics.Metric) (query string, err error) {
+	switch m.Type {
 	case metrics.Counter:
 		query = `
 			INSERT INTO metrics (name, type, counter, gauge) VALUES ($1, $2, $3, $4) 
