f004267fad8201b4da0b5dae4e90f4d233b9edc3
crazylazyowl <58094513+crazylazyowl@users.noreply.github.com>
2025-07-08 17:12:33 +0200

    rewrite agent retries

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -17,36 +18,28 @@ import (
 	"github.com/crazylazyowl/metrics-tpl/internal/usecase/metrics"
 )
 
-const (
-// baseURL    = "http://localhost:8080/update"
-// gaugeURL   = "http://%s/update/gauge/%s/%f"
-// counterURL = "http://%s/update/counter/%s/%d"
-)
-
 func main() {
 	conf, err := loadConfig()
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
-
-	monitor(ctx, conf)
+	monitor(ctx, conf.address, conf.pollInterval, conf.reportInterval)
 }
 
-func monitor(ctx context.Context, conf *config) error {
+func monitor(ctx context.Context, address string, pollInterval, reportInterval int) error {
 	gauge := make(map[string]float64)
 	var counter int64
-	url := fmt.Sprintf("http://%s/update/", conf.address)
-	retries := 3
+	// NOTE: hack for second test
+	attempts := 4
 	delay := 1
-	if err := ping(url); err != nil { // NOTE: hack for second test
-		retries = 1
+	if err := tryPing(address); err != nil {
+		attempts = 1
 		delay = 0
 	}
-	pollTicker := time.NewTicker(time.Duration(conf.pollInterval) * time.Second)
-	reportTicker := time.NewTicker(time.Duration(conf.reportInterval) * time.Second)
+	pollTicker := time.NewTicker(time.Duration(pollInterval) * time.Second)
+	reportTicker := time.NewTicker(time.Duration(reportInterval) * time.Second)
 	for {
 		select {
 		case <-ctx.Done():
@@ -94,7 +87,7 @@ func monitor(ctx context.Context, conf *config) error {
 					Gauge: &value,
 				}
 				// many = append(many, metric)
-				if err := report(url, &metric, retries, delay); err != nil {
+				if err := reportOne(address, metric, attempts, delay); err != nil {
 					log.Printf("failed to send %s (%f); err=%v\n", key, value, err)
 				}
 			}
@@ -103,109 +96,85 @@ func monitor(ctx context.Context, conf *config) error {
 				Type:    metrics.Counter,
 				Counter: &counter,
 			}
-			if err := report(url, &metric, retries, delay); err != nil {
+			if err := reportOne(address, metric, attempts, delay); err != nil {
 				log.Printf("failed to send %s (%d); err=%v\n", "PollCount", counter, err)
 			}
 			// many = append(many, metric)
-			// if err := reportBulk(url, many); err != nil {
+			// if err := reportMany(address, many, attempts, delay); err != nil {
 			// 	log.Printf("failed to bulk metrics; err=%v\n", err)
 			// }
 		}
 	}
 }
 
-func ping(url string) error {
-	resp, err := http.Get(url)
+func tryPing(address string) error {
+	url := fmt.Sprintf("http://%s/update/", address)
+	resp, err := http.Head(url)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	resp.Body.Close()
 	return nil
 }
 
-func report(url string, metric *metrics.Metric, retries int, delay int) error {
-	// if err := metric.Validate(); err != nil {
-	// 	return err
-	// }
-
-	data, err := json.Marshal(metric)
-	if err != nil {
-		return err
-	}
-
-	// buf := bytes.NewBuffer(nil)
-
-	// w := gzip.NewWriter(buf)
-	// w.Write(data)
-	// w.Close()
-
-	n := delay
-
-	for i := 0; i < retries; i++ {
-		req, _ := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
-		req.Header.Add("Content-Type", "application/json")
-
-		if err = sendRequest(req); err != nil {
-			log.Printf("retry #%d\n", i)
-			time.Sleep(time.Duration(n) * time.Second)
-			n += 2
-		} else {
-			break
+func reportMany(address string, mm []metrics.Metric, attempts int, delay int) error {
+	for _, m := range mm {
+		if err := m.Validate(); err != nil {
+			return err
 		}
 	}
-
-	return err
-}
-
-func sendRequest(req *http.Request) error {
-	// req, _ := http.NewRequest(http.MethodPost, url, buf)
-	// req.Header.Add("Content-Encoding", "gzip")
-	resp, err := http.DefaultClient.Do(req)
+	data, err := json.Marshal(mm)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("status code = %d, data = %s", resp.StatusCode, string(body))
-	}
-
-	return nil
+	return report(fmt.Sprintf("http://%s/updates/", address), data, attempts, delay, true)
 }
 
-// func reportBulk(url string, many []metrics.Metric) error {
-// 	// buf := bytes.NewBuffer(nil)
-// 	// w := gzip.NewWriter(buf)
-// 	// if err := json.NewEncoder(w).Encode(many); err != nil {
-// 	// 	return err
-// 	// }
-// 	// w.Close()
+func reportOne(address string, m metrics.Metric, attempts int, delay int) error {
+	if err := m.Validate(); err != nil {
+		return err
+	}
+	data, err := json.Marshal(&m)
+	if err != nil {
+		return err
+	}
+	return report(fmt.Sprintf("http://%s/update/", address), data, attempts, delay, false)
+}
 
-// 	data, err := json.Marshal(many)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// log.Printf(string(data))
-
-// 	// req, _ := http.NewRequest(http.MethodPost, url, buf)
-// 	req, _ := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
-// 	req.Header.Add("Content-Type", "application/json")
-// 	// req.Header.Add("Content-Encoding", "gzip")
-
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	body, _ := io.ReadAll(resp.Body)
-// 	resp.Body.Close()
-
-// 	if resp.StatusCode != 200 {
-// 		return fmt.Errorf("status code = %d, data = %s", resp.StatusCode, string(body))
-// 	}
-
-// 	return nil
-
-// }
+func report(url string, data []byte, attempts, delay int, compress bool) error {
+	var err error
+	if compress {
+		log.Printf("compression is on")
+		buf := bytes.NewBuffer(nil)
+		w := gzip.NewWriter(buf)
+		w.Write(data)
+		w.Close()
+		data = buf.Bytes()
+	}
+	for range attempts {
+		var req *http.Request
+		req, err = http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+		if err != nil {
+			return fmt.Errorf("failed to prepare request; %w", err)
+		}
+		var resp *http.Response
+		req.Header.Add("Content-Type", "application/json")
+		if compress {
+			req.Header.Add("Content-Encoding", "gzip")
+		}
+		resp, err = http.DefaultClient.Do(req)
+		if err != nil {
+			log.Printf("retry report")
+			time.Sleep(time.Duration(delay) * time.Second)
+			delay += 2
+			continue
+		}
+		body, _ := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("status code = %d, data = %s", resp.StatusCode, string(body))
+		}
+		break
+	}
+	return err
+}
