692befedce001f084bd4e846327994ba9919fc06
crazylazyowl <58094513+crazylazyowl@users.noreply.github.com>
2025-07-19 18:02:00 +0200

    add mw that sign response body with hmac

--- internal/controller/httprest/middleware/hmac.go
+++ internal/controller/httprest/middleware/hmac.go
@@ -10,10 +10,10 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func CheckHMAC(secret string) func(next http.Handler) http.Handler {
+func CheckHMAC(secret string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if have := r.Header.Get("HashSHA256"); have != "" {
+			if hmac := r.Header.Get("HashSHA256"); hmac != "" {
 				logger := log.With().Logger()
 				data, err := io.ReadAll(r.Body)
 				if err != nil {
@@ -21,8 +21,7 @@ func CheckHMAC(secret string) func(next http.Handler) http.Handler {
 					w.WriteHeader(http.StatusInternalServerError)
 					return
 				}
-				want := security.HMACString([]byte(secret), data)
-				if have != want {
+				if hmac != security.HMACString([]byte(secret), data) {
 					logger.Error().Err(err).Msg("hash is invalid")
 					w.WriteHeader(http.StatusBadRequest)
 					return
@@ -31,6 +30,37 @@ func CheckHMAC(secret string) func(next http.Handler) http.Handler {
 				r.Body = io.NopCloser(bytes.NewReader(data))
 			}
 			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+type hmacResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	buf        *bytes.Buffer
+}
+
+func (w *hmacResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+}
+
+func (w *hmacResponseWriter) Write(data []byte) (int, error) {
+	return w.buf.Write(data)
+}
+
+func AddHMAC(secret string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			logger := log.With().Logger()
+			rw := hmacResponseWriter{ResponseWriter: w, buf: bytes.NewBuffer(nil)}
+			next.ServeHTTP(&rw, r)
+			hmac := security.HMACString([]byte(secret), rw.buf.Bytes())
+			w.Header().Set("HashSHA256", hmac)
+			w.WriteHeader(rw.statusCode)
+			_, err := w.Write(rw.buf.Bytes())
+			if err != nil {
+				logger.Error().Err(err).Msg("failed to write response body")
+			}
 		})
 	}
 }
